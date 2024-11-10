56eef625b945077f0f7d3806553c557d913ae17c
emferrari31 <e.ferrari@live.co.uk>
2024-11-10 11:42:50 +0000

    tidied up fonts

--- src/components/WhyUsSection/index.jsx
+++ src/components/WhyUsSection/index.jsx
@@ -2,10 +2,10 @@ import './styles.css';
 
 const WhyUsSection = () => {
     const reasons = [
-        {id: 1, name: 'PERFECTLY PORTIONED', description: 'Our cans hold the equivalent of one large glass, or two smaller glasses, allowing for a perfect serve every time.', image: "public/perfectlyPortioned.jpg"},
-        {id: 2, name: 'WINEMAKER PARTNERSHIPS', description: 'We work with some of Europe’s finest winemakers. They make fantastic wines, we protect that quality.', image: "public/winemakerPartnership.jpg"},
-        {id: 3, name: 'EXCITING GRAPE VARIETY', description: 'Discovery of some of the best and most unique wines that Europe has to offer, capturing a sense of place and time.', image: "public/excitingGrape.jpg"},
-        {id: 4, name: 'INFINITELY RECYCLABLE', description: 'As a funding partner of Every Can Counts we help promote wider, responsible aluminium recycling.', image: "public/recycle.jpg"},
+        {id: 1, name: 'PERFECTLY PORTIONED', description: 'Our cans hold the equivalent of one large glass, or two smaller glasses, allowing for a perfect serve every time.', image: "public/perfectlyPortioned.webp"},
+        {id: 2, name: 'WINEMAKER PARTNERSHIPS', description: 'We work with some of Europe’s finest winemakers. They make fantastic wines, we protect that quality.', image: "public/winemakerPartnership.webp"},
+        {id: 3, name: 'EXCITING GRAPE VARIETY', description: 'Discovery of some of the best and most unique wines that Europe has to offer, capturing a sense of place and time.', image: "public/excitingGrape.webp"},
+        {id: 4, name: 'INFINITELY RECYCLABLE', description: 'As a funding partner of Every Can Counts we help promote wider, responsible aluminium recycling.', image: "public/recycle.webp"},
     ]
 
     return (
@@ -20,7 +20,7 @@ const WhyUsSection = () => {
                     <div key={reason.id}>
                         <img src={reason.image} alt={reason.name} className="product-image"/>
                         <h3>{reason.name}</h3>
-                        <h4>{reason.description}</h4>
+                        <p>{reason.description}</p>
                     </div>
                 ))}
 
