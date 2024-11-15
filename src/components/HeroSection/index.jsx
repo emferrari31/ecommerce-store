9037cfe82e0fab3c3f5d8865f5550a60e643015a
emferrari31 <e.ferrari@live.co.uk>
2024-11-15 14:19:15 +0000

    changed pathways of two svgs to test for plesk

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -7,12 +7,12 @@ const HeroSection = () => {
                 <h1>EXQUISITE OLIVE OIL <br/>
                 HARVESTED FROM TUSCANY</h1>
                 <div className='gold-underline-container'>
-                    <img src="public/gold-ul.svg" alt='gold underline' className="gold-underline"/>
+                    <img src="gold-ul.svg" alt='gold underline' className="gold-underline"/>
                 </div>
                 <p>At Terra Tuscana, we’re dedicated to bringing the authentic taste of Tuscany to your table.
                     Our olive oil is crafted with care, from handpicked olives grown in the rolling hills of
                     Italy’s most beloved region.</p>
-                <img src="public/5stars.svg" alt="5 star rating image" className="five-star-rating"/>
+                <img src="5stars.svg" alt="5 star rating image" className="five-star-rating"/>
                 <button className="cta-btn">
                     <Link to="/products" style={{ textDecoration: 'none' }}>
                         Shop all
